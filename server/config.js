144bc32601c2ae3351a712c67edf8b9416afd145
Alexis Santini <santini.alexis@gmail.com>
2019-01-31 16:00:02 +0100

    fix

--- server/config.js
+++ server/config.js
@@ -5,7 +5,7 @@ const config = {};
 config.PORT = process.env.PORT || 3000;
 
 if (process.env.NODE_ENV === 'development') {
-  config.MONGODB_URI = process.env.MONGODB_URI_ENV;
+  config.MONGODB_URI = process.env.MONGODB_URI_DEV;
 }
 
 if (process.env.NODE_ENV === 'production') {
